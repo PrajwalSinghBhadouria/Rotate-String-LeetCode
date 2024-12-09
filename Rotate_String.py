7d2e94292cfe7301df0cd2c0db4d6102f818cd7d
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-12-09 20:44:16 +0530

    Add files via upload

---
+++ Rotate_String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal):
+            return False
+        st = s+s;
+        if goal in st:
+            return True
+        return False
+        
